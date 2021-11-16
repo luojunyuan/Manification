8940281fb596abac410fa123b0ae7c111508940a
k1mlka <k1mlka@outlook.com>
2021-11-16 14:44:25 +0800

    添加项目文件。

---
+++ Manification/Form1.cs
@@ -0,0 +1,15 @@
+namespace Manification
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            new Magnifier(this);
+        }
+    }
+}
